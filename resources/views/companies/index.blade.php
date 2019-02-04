8c9d5d4611e84084b1f377e9c46fb021ef9ada9b
Luiz Honorato <lhehonorato@gmail.com>
2019-02-04 12:04:40 -0200

    Link na lista de sites das empresas.

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -41,7 +41,7 @@
                 <td><img src="{{ url("storage/logos/{$companie->logo}") }}" class="img-thumbnail" style="width: 90px; height: 90px;"></td>
                 <td>{{$companie->name}}</td>
                 <td>{{$companie->email}}</td>
-                <td>{{$companie->site}}</td>
+                <td><a href="http://{{$companie->site}}" target="_blank">{{$companie->site}}</a></td>
                 <td>
                     <form action="{{ route('companies.destroy', $companie->id)}}" method="post">
                         <a href="{{ route('companies.edit', $companie->id)}}" class="btn btn-warning">
